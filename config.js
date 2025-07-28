3a79c29a6aac588b100ff6f7324396f82982c8da
Bahabey <157840633+Bahattin421213@users.noreply.github.com>
2025-07-28 22:22:39 +0300

    GİZEM

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "BACIM DOĞUM GÜNÜN KUTLU OLSUNN!",
+    introTitle: 'GİZEM DOĞDUNNN SONUNDAA!',
+    introDesc: `DOĞUM GÜNÜN KUTLU OLSUN İYİ Kİ DOĞMUŞSUN İYİ Kİ SENİNLE 
+    TANIŞMIŞIMM`,
+    btnIntro: '^^BASS^^',
+    title: 'DOĞUM GÜNÜN KUTLU OLSUN BACIMM   ' 🥰',
+    desc: 'SİTEYİ BEĞENDİN Mİ MUTLU MUSUUN ',
+    btnYes: 'BEĞENDİM MUTLUYUMM',
+    btnNo: 'DEĞİLİM SG',
+    question: 'söylemek istediğin bişey var mı cücee',
+    btnReply: 'Heh sağol',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'İYİ Kİ DOĞDUNNNN İYİ Kİ VARSINN',
+    messDesc: 'Şey bundan sonra KONYALI demek yok',
+    btnAccept: 'Kabull(yokbaşkasecenek)',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
